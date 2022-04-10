2d93b5dcb468d2fa72c96326c23d00b0fb6a1959
Carlos Actis <67552637+charlyuni@users.noreply.github.com>
2022-04-10 12:49:59 -0300

    entregav2

--- coderdjango/coderdjango/template_ecommerce/models.py
+++ coderdjango/coderdjango/template_ecommerce/models.py
@@ -7,3 +7,16 @@ class formulario(models.Model):
     email_client = models.EmailField(max_length=254, default="Some String")
     phone = models.IntegerField()
     messege = models.CharField(max_length=20)
+
+class producto(models.Model):
+    name = models.CharField(max_length=40)
+    descripcion = models.CharField(max_length=40)
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+    categoria = models.CharField(max_length=40)
+    imagen= models.CharField(max_length=40)
+
+class Carrito(models.Model):
+    total = models.CharField(max_length=40)
+    fecha_compra = models.DateField()
+    tipo_envio  = models.CharField(max_length=40)
